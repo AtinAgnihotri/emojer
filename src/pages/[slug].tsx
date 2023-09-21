39f12f60310618861f480c728f44dfa67d534d07
Atin Agnihotri <atinjiagnihotri@gmail.com>
2023-09-21 23:38:14 +0530

    add profile and post route

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -8,7 +8,6 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { UserImage } from "~/components/Profile";
 import { LoadingPage, LoadingSpinner } from "~/components/Loading";
-import Link from "next/link";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
@@ -69,22 +68,13 @@ const CreatePostWizard = () => {
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 
 const PostView: React.FC<PostWithUser> = ({ post, author }) => {
-  const userSlug = `/@${author.name}`;
-  const postSlug = `/post/${post.id}`;
-
   return (
     <div className="flex flex-row items-center border-b border-slate-400 p-8 ">
-      <Link href={userSlug}>
-        <UserImage url={author.profilePicture} userName={author.name} />
-      </Link>
+      <UserImage url={author.profilePicture} userName={author.name} />
       <div className="flex flex-col pl-4">
         <div className="flex gap-1">
-          <Link href={userSlug}>
-            <span>@{author.name}</span>
-          </Link>
-          <Link href={postSlug}>
-            <span>{`· ${dayjs(post.createdAt).fromNow()}`}</span>
-          </Link>
+          <span>@{author.name}</span>
+          <span>{`· ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
         <span className="pt-1 text-2xl">{post.content}</span>
       </div>
@@ -125,15 +115,7 @@ export default function Home() {
       </Head>
       <main className="flex h-full min-h-screen justify-center">
         <div className="w-full flex-col border-x border-slate-400  md:max-w-2xl">
-          <div
-            className={`flex ${
-              user.isSignedIn ? "justify-between" : "justify-center"
-            } border-b border-slate-400 p-4`}
-          >
-            {!!user.isSignedIn && <CreatePostWizard />}
-            {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
-          </div>
-          {!!user.isSignedIn && <PostsFeed />}
+          Profile view
         </div>
       </main>
     </>
